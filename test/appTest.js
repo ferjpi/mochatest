ff790108ba405d799b47bd44a90ee8177ef0d6bc
ferjpi <fernandojpt1203@gmail.com>
2018-07-25 15:53:19 -0500

    function break in diferents testing

--- test/appTest.js
+++ test/appTest.js
@@ -1,16 +1,34 @@
 const assert = require('chai').assert
-const sayHello = require('../app').sayHello
-const addNumber = require('../app').addNumbers
-// const app = require('../app')
+// const sayHello = require('../app').sayHello
+// const addNumber = require('../app').addNumbers
+const app = require('../app')
+
+// Results
+const sayHelloResult = app.sayHello()
+const addNumbersResult = app.addNumbers(5,5)
 
 describe('App', function() {
-  it('sayHello should return hello', function() {
-    let result = sayHello()
-    assert.equal(result, 'hello')
+  describe('sayHello()', function() {
+    it('sayHello should return hello', function() {
+      // let result = app.sayHello()
+      assert.equal(sayHelloResult, 'hello')
+    })
+
+    it('sayHello should return type string', function() {
+      // let result = app.sayHello()
+      assert.typeOf(sayHelloResult, 'string')
+    })
   })
 
-  it('sayHello should return type string', function() {
-    let result = sayHello()
-    assert.typeOf(result, 'string')
+  describe('addNumbers()', function() {
+    it('addNumber should be above 5', function() {
+      // let result = app.addNumber(5,5)
+      assert.isAbove(addNumbersResult, 5)
+    })
+
+    it('addNumber should return type number', function() {
+      // let result = app.addNumber()
+      assert.typeOf(addNumbersResult, 'number')
+    })
   })
 })
